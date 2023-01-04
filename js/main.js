e608fa844ed826946b73740eac12ccf482397759
Vinícius Yoda <vyoda4604@gmail.com>
2023-01-04 12:38:26 -0300

    map

--- js/main.js
+++ js/main.js
@@ -4,26 +4,34 @@ getBuscarLivrosDaApi()
 const elementoParaInserirLivros = document.getElementById('livros')
 
 async function getBuscarLivrosDaApi() {
-   const res = await fetch(endpointDaApi)
-   livros = await res.json()
-   console.table(livros);
-   exibirOsLivrosNaTela(livros)
+  const res = await fetch(endpointDaApi)
+  livros = await res.json()
+  let livrosComDescontos = aplicarDesconto(livros)
+  exibirOsLivrosNaTela(livrosComDescontos)
 }
 
 function exibirOsLivrosNaTela(listaDeLivros) {
-   listaDeLivros.forEach(livro => {
-      elementoParaInserirLivros.innerHTML += `
+  listaDeLivros.forEach(livro => {
+    elementoParaInserirLivros.innerHTML += `
       <div class="livro">
       <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
       <h2 class="livro__titulo">
         ${livro.titulo}
       </h2>
       <p class="livro__descricao">${livro.autor}</p>
-      <p class="livro__preco" id="preco">${livro.preco}</p>
+      <p class="livro__preco" id="preco">${livro.preco.toFixed(2)}</p>
       <div class="tags">
         <span class="tag">${livro.categoria}</span>
       </div>
     </div>
       `
-   })
+  })
+}
+
+function aplicarDesconto(livros) {
+  const desconto = 0.3
+  livrosComDescontos = livros.map(livro => {
+    return {...livro, preco: livro.preco - (livro.preco * desconto)}
+  })
+  return livrosComDescontos
 }
